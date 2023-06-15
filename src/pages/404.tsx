226cfc7ca7fb552c20160c6355a7dd64458ea4c2
bruno.garcia <bruno.silva@trocafone.com>
2023-06-14 23:13:19 -0300

    fix: 404 page

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,21 +1,24 @@
 import React from 'react';
 import Link from 'next/link';
+import { GetStaticProps } from 'next/types';
+import { useTranslations } from 'next-intl';
 
 import Layout from '@/components/layout/Layout';
 
-const Error = () => {
+export default function Error() {
+  const errorPageTranslations = useTranslations('NotFoundPage');
   return (
     <Layout>
-      <div className="pt-120 pb-120">
+      <div className="pt-120 pb-120 d-flex justify-content-center align-items-center" style={{ height: '94vh' }}>
         <div className="container">
           <div className="row justify-content-center">
             <div className="col-12 col-lg-6 text-center">
-              <h2 className="mb-35">Page Not Found</h2>
+              <h2 className="mb-35">{errorPageTranslations('Title')}</h2>
               <Link
                 href="/"
-                className="border-btn text-dark bg-warning border border-dark text-center ms-3 borderc-btn d-inline-flex"
+                className="border-btn text-dark bg-warning border border-dark text-center mt-20 ms-3 borderc-btn d-inline-flex"
               >
-                Back To Home
+                {errorPageTranslations('ContentButton')}
               </Link>
             </div>
           </div>
@@ -23,6 +26,13 @@ const Error = () => {
       </div>
     </Layout>
   );
-};
+}
 
-export default Error;
+export const getStaticProps: GetStaticProps = async ({ locale }) => {
+  return {
+    props: {
+      messages: require(`../../i18n/${locale}.json`),
+      redirect: '/',
+    },
+  };
+};
